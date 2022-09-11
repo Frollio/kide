a860b6531fce27317143b5fdbd39b8dee7c10914
Olli <ronkolli@gmail.com>
2022-09-11 09:21:50 +0300

    Minor changes

--- functions.php
+++ functions.php
@@ -1,9 +1,21 @@
 <?php
 
 if ( ! function_exists( 'kide_setup' ) ) :
+
   function kide_setup() {
     add_theme_support( 'wp-block-styles' );
-    add_editor_style( 'editor-style.css' );
+    add_editor_style( 'style.css' );
+  }
+
+endif;
+
+add_action( 'after_setup_theme', 'kide_setup' );
+
+if ( ! function_exists( 'kide_styles' ) ) :
+
+  function kide_styles() {
+    wp_enqueue_style( 'style', get_stylesheet_uri() );
   }
 endif;
-add_action( 'after_setup_theme', 'kide_setup' );
+
+add_action( 'wp_enqueue_scripts', 'kide_styles' );
