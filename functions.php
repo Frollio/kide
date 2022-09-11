39b5372f0a41e4dddfe59573b37af406bda32a28
Olli <ronkolli@gmail.com>
2022-09-11 09:47:17 +0300

    Added Normalizer

--- functions.php
+++ functions.php
@@ -14,6 +14,8 @@ add_action( 'after_setup_theme', 'kide_setup' );
 if ( ! function_exists( 'kide_styles' ) ) :
 
   function kide_styles() {
+    wp_enqueue_style( 'normalize', get_template_directory_uri() . 
+      '/assets/css/normalize.css', false, '8.0.1', 'all' );
     wp_enqueue_style( 'style', get_stylesheet_uri() );
   }
 endif;
