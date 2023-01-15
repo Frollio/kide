8517e15b27e4a621e2d58393061871de68f6afb4
Olli <ronkolli@gmail.com>
2023-01-15 11:36:32 +0200

    Rearranged the code

--- patterns/introduction-site.php
+++ patterns/introduction-site.php
@@ -8,21 +8,34 @@
 ?>
 
 <!-- wp:group {"tagName":"section","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center"}} -->
-<section class="wp-block-group"><!-- wp:columns -->
-<div class="wp-block-columns"><!-- wp:column -->
-<div class="wp-block-column"><!-- wp:heading -->
-<h2 id="introduction-heading">Introduction heading</h2>
-<!-- /wp:heading -->
+<section class="wp-block-group">
+  
+  <!-- wp:columns -->
+  <div class="wp-block-columns">
+  
+    <!-- wp:column -->
+    <div class="wp-block-column">
+      <!-- wp:heading -->
+      <h2 id="introduction-heading">Introduction heading</h2>
+      <!-- /wp:heading -->
+      <!-- wp:paragraph -->
+      <p>Your introduction text for the end-user to understand what this page is about.</p>
+      <!-- /wp:paragraph -->
+    </div>
+    <!-- /wp:column -->
 
-<!-- wp:paragraph -->
-<p>Your introduction text for the end-user to understand what this page is about.</p>
-<!-- /wp:paragraph --></div>
-<!-- /wp:column -->
+    <!-- wp:column -->
+    <div class="wp-block-column">
+      <!-- wp:image {"id":"introduction-site-image","sizeSlug":"large","linkDestination":"none"} -->
+      <figure class="wp-block-image size-large">
+        <img src="<?php echo esc_url( get_theme_file_uri( 'assets/images/3x1_2000x666.png' ) ); ?>" alt="ADD YOUR IMAGE HERE"/>
+      </figure>
+      <!-- /wp:image -->
+    </div>
+    <!-- /wp:column -->
 
-<!-- wp:column -->
-<div class="wp-block-column"><!-- wp:image {"id":"introduction-site-image","sizeSlug":"large","linkDestination":"none"} -->
-<figure class="wp-block-image size-large"><img src="<?php echo esc_url( get_theme_file_uri( 'assets/images/3x1_2000x666.png' ) ); ?>" alt="ADD YOUR IMAGE HERE"/></figure>
-<!-- /wp:image --></div>
-<!-- /wp:column --></div>
-<!-- /wp:columns --></section>
+  </div>
+  <!-- /wp:columns -->
+
+</section>
 <!-- /wp:group -->
