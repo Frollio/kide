6a2a6e1e7ec054b2c373942a70a1dfe7a2ec1b59
Olli <ronkolli@gmail.com>
2023-01-21 11:09:45 +0200

    Fixed element overflow

--- patterns/introduction-site.php
+++ patterns/introduction-site.php
@@ -13,19 +13,19 @@
   <!-- wp:columns {"className":"kide-site-intro-columns"} -->
   <div class="wp-block-columns kide-site-intro-columns">
   
-    <!-- wp:column {"className":"kide-site-intro-column} -->
-    <div id="kide-intro-col-1" class="wp-block-column kide-site-intro-column">
-      <!-- wp:heading {"textAlign":"center"} -->
-      <h2 class="has-text-align-center" id="introduction-heading">Introduction heading</h2>
+    <!-- wp:column {"verticalAlignment":"center","className":"kide-site-intro-column} -->
+    <div id="kide-intro-col-1" class="wp-block-column is-vertically-aligned-center kide-site-intro-column">
+      <!-- wp:heading {"textAlign":"center","fontSize":"xxxl"} -->
+      <h2 class="has-text-align-center has-xxxl-font-size" id="introduction-heading">Introduction heading</h2>
       <!-- /wp:heading -->
-      <!-- wp:paragraph {"align":"center"} -->
-      <p class="has-text-align-center">Your introduction text for the end-user to understand what this page is about.</p>
+      <!-- wp:paragraph {"align":"center","fontSize":"m"} -->
+      <p class="has-text-align-center has-m-font-size">Your introduction text for the end-user to understand what this page is about.</p>
       <!-- /wp:paragraph -->
     </div>
     <!-- /wp:column -->
 
-    <!-- wp:column {"className":"kide-site-intro-column} -->
-    <div id="kide-intro-col-2" class="wp-block-column kide-site-intro-column">
+    <!-- wp:column {"verticalAlignment":"center","className":"kide-site-intro-column} -->
+    <div id="kide-intro-col-2" class="wp-block-column is-vertically-aligned-center kide-site-intro-column">
       <!-- wp:image {"id":"introduction-site-image","sizeSlug":"large","linkDestination":"none"} -->
       <figure class="wp-block-image size-large">
         <img src="<?php echo esc_url( get_theme_file_uri( 'assets/images/3x1_2000x666.png' ) ); ?>" alt="ADD YOUR IMAGE HERE"/>
