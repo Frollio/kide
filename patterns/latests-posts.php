7a80351ca195d72d1de083c047e3567da25cc7eb
Olli <ronkolli@gmail.com>
2023-01-22 15:23:19 +0200

    Centered posts list and added heading

--- patterns/latests-posts.php
+++ patterns/latests-posts.php
@@ -7,6 +7,22 @@
    * */
 ?>
 
-<!-- wp:group {"layout":{"type":"default"}} -->
-<div class="wp-block-group"><!-- wp:latest-posts {"postsToShow":10,"displayPostDate":true} /--></div>
+<!-- wp:group {"tagName":"section","backgroundColor":"background3","layout":{"type":"default"}} -->
+<section class="wp-block-group has-background-3-background-color has-background">
+  
+<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group">
+  <!-- wp:heading -->
+  <h2 id="latests-posts">Latests posts</h2>
+  <!-- /wp:heading -->
+</div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center","orientation":"horizontal"}} -->
+<div class="wp-block-group">
+  <!-- wp:latest-posts {"postsToShow":10,"displayPostDate":true} /-->
+</div>
+<!-- /wp:group -->
+
+</section>
 <!-- /wp:group -->
