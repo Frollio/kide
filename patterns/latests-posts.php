dedbcef1bf3edea18506003843f57c070ac335e2
Olli <ronkolli@gmail.com>
2023-01-22 14:38:26 +0200

    Added latests posts pattern

---
+++ patterns/latests-posts.php
@@ -0,0 +1,12 @@
+
+<?php
+  /**
+   * Title: Latests posts with headings and dates
+   * slug: kide/latests-posts
+   * Categories: text
+   * */
+?>
+
+<!-- wp:group {"layout":{"type":"default"}} -->
+<div class="wp-block-group"><!-- wp:latest-posts {"postsToShow":10,"displayPostDate":true} /--></div>
+<!-- /wp:group -->
