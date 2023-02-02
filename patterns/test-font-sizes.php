8c8b873df9707ec43e498361be180a7bf39485df
Olli <ronkolli@gmail.com>
2023-02-02 17:33:01 +0200

    Added a test pattern for font sizes

---
+++ patterns/test-font-sizes.php
@@ -0,0 +1,42 @@
+
+<?php
+  /**
+   * Title: A section to test font sizes
+   * slug: kide/test-font-sizes
+   * Categories: text
+   * */
+?>
+
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group">
+  
+  <!-- wp:heading {"level":1} -->
+  <h1 id="h1-otsikko">H1 Otsikko</h1>
+  <!-- /wp:heading -->
+
+  <!-- wp:heading -->
+  <h2 id="h2-otsikko">H2 Otsikko</h2>
+  <!-- /wp:heading -->
+
+  <!-- wp:heading {"level":3} -->
+  <h3 id="h3-otsikko">H3 Otsikko</h3>
+  <!-- /wp:heading -->
+
+  <!-- wp:heading {"level":4} -->
+  <h4 id="h4-otsikko">H4 Otsikko</h4>
+  <!-- /wp:heading -->
+
+  <!-- wp:heading {"level":5} -->
+  <h5 id="h5-otsikko">H5 Otsikko</h5>
+  <!-- /wp:heading -->
+
+  <!-- wp:heading {"level":6} -->
+  <h6 id="h6-otsikko">H6 Otsikko</h6>
+  <!-- /wp:heading -->
+
+  <!-- wp:paragraph -->
+  <p>Kappale: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  <!-- /wp:paragraph -->
+
+</div>
+<!-- /wp:group -->
