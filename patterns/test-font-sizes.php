0251706310d17ce8bbe9c0ce433c078f74143478
Olli <ronkolli@gmail.com>
2023-02-04 13:46:34 +0200

    Added more test paragraphs

--- patterns/test-font-sizes.php
+++ patterns/test-font-sizes.php
@@ -14,29 +14,61 @@
   <h2 class="has-5-xl-font-size" id="h1-5xl-otsikko">H1 5XL Otsikko potsikko pusikko asiaton loton poton pytyn</h2>
   <!-- /wp:heading -->
 
+  <!-- wp:paragraph -->
+  <p>Kappale: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  <!-- /wp:paragraph -->
+
   <!-- wp:heading {"level":1} -->
   <h1 id="h1-otsikko">H1 Otsikko potsikko pusikko asiaton loton poton pytyn</h1>
   <!-- /wp:heading -->
+
+  <!-- wp:paragraph -->
+  <p>Kappale: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  <!-- /wp:paragraph -->
 
   <!-- wp:heading -->
   <h2 id="h2-otsikko">H2 Otsikko potsikko pusikko asiaton loton poton pytyn</h2>
   <!-- /wp:heading -->
 
+  <!-- wp:paragraph -->
+  <p>Kappale: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  <!-- /wp:paragraph -->
+
   <!-- wp:heading {"level":3} -->
   <h3 id="h3-otsikko">H3 Otsikko potsikko pusikko asiaton loton poton pytyn</h3>
   <!-- /wp:heading -->
+
+  <!-- wp:paragraph -->
+  <p>Kappale: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  <!-- /wp:paragraph -->
 
   <!-- wp:heading {"level":4} -->
   <h4 id="h4-otsikko">H4 Otsikko potsikko pusikko asiaton loton poton pytyn Otsikko potsikko pusikko asiaton loton poton pytyn</h4>
   <!-- /wp:heading -->
 
+  <!-- wp:paragraph -->
+  <p>Kappale: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  <!-- /wp:paragraph -->
+
   <!-- wp:heading {"level":5} -->
   <h5 id="h5-otsikko">H5 Otsikko potsikko pusikko asiaton loton poton pytyn Otsikko potsikko pusikko asiaton loton poton pytyn</h5>
   <!-- /wp:heading -->
 
+  <!-- wp:paragraph -->
+  <p>Kappale: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  <!-- /wp:paragraph -->
+
   <!-- wp:heading {"level":6} -->
   <h6 id="h6-otsikko">H6 Otsikko potsikko pusikko asiaton loton poton pytyn Otsikko potsikko pusikko asiaton loton poton pytyn</h6>
   <!-- /wp:heading -->
+
+  <!-- wp:paragraph -->
+  <p>Kappale: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  <!-- /wp:paragraph -->
+
+  <!-- wp:paragraph -->
+  <p>Kappale: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  <!-- /wp:paragraph -->
 
   <!-- wp:paragraph -->
   <p>Kappale: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
