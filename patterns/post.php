c5ffe0859de0dbce519224a8d21b6112f900d7eb
Olli <ronkolli@gmail.com>
2023-02-05 11:44:44 +0200

    Added post pattern

---
+++ patterns/post.php
@@ -0,0 +1,24 @@
+
+<?php
+  /**
+   * Title: Post title, date, author, featured image and content
+   * slug: kide/post
+   * Categories: text, image
+   * */
+?>
+
+<!-- wp:group {"tagName":"section","className":"kide-post","layout":{"type":"constrained","contentSize":"1200px"}} -->
+<section class="wp-block-group kide-post">
+  
+  <!-- wp:post-title {"level":1} /-->
+
+  <!-- wp:post-date /-->
+
+  <!-- wp:post-author {"showBio":false} /-->
+
+  <!-- wp:post-featured-image /-->
+
+  <!-- wp:post-content /-->
+
+</section>
+<!-- /wp:group -->
