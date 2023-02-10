7d1c13fca1401fa6284b6e1b4c3c7ff93e27aa53
Olli <ronkolli@gmail.com>
2023-02-10 18:46:27 +0200

    Added padding to test font pattern

--- patterns/test-font-sizes.php
+++ patterns/test-font-sizes.php
@@ -9,78 +9,84 @@
 
 <!-- wp:group {"layout":{"type":"constrained","wideSize":"1000px"}} -->
 <div class="wp-block-group">
-  
-  <!-- wp:heading {"fontSize":"5-xl"} -->
-  <h2 class="has-5-xl-font-size" id="h1-5xl-otsikko">H1 5XL Otsikko potsikko pusikko asiaton loton poton pytyn</h2>
-  <!-- /wp:heading -->
 
-  <!-- wp:paragraph -->
-  <p>Kappale: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-  <!-- /wp:paragraph -->
+  <!-- wp:group {"className":"kide-test-font-container","layout":{"type":"flex","orientation":"vertical"}} -->
+  <div class="wp-block-group kide-test-font-container">
+    
+    <!-- wp:heading {"fontSize":"5-xl"} -->
+    <h2 class="has-5-xl-font-size" id="h1-5xl-otsikko">H1 5XL Otsikko potsikko pusikko asiaton loton poton pytyn</h2>
+    <!-- /wp:heading -->
 
-  <!-- wp:heading {"level":1} -->
-  <h1 id="h1-otsikko">H1 Otsikko potsikko pusikko asiaton loton poton pytyn</h1>
-  <!-- /wp:heading -->
+    <!-- wp:paragraph -->
+    <p>Kappale: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <!-- /wp:paragraph -->
 
-  <!-- wp:paragraph -->
-  <p>Kappale: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-  <!-- /wp:paragraph -->
+    <!-- wp:heading {"level":1} -->
+    <h1 id="h1-otsikko">H1 Otsikko potsikko pusikko asiaton loton poton pytyn</h1>
+    <!-- /wp:heading -->
 
-  <!-- wp:heading -->
-  <h2 id="h2-otsikko">H2 Otsikko potsikko pusikko asiaton loton poton pytyn</h2>
-  <!-- /wp:heading -->
+    <!-- wp:paragraph -->
+    <p>Kappale: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <!-- /wp:paragraph -->
 
-  <!-- wp:paragraph -->
-  <p>Kappale: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-  <!-- /wp:paragraph -->
+    <!-- wp:heading -->
+    <h2 id="h2-otsikko">H2 Otsikko potsikko pusikko asiaton loton poton pytyn</h2>
+    <!-- /wp:heading -->
 
-  <!-- wp:heading {"level":3} -->
-  <h3 id="h3-otsikko">H3 Otsikko potsikko pusikko asiaton loton poton pytyn</h3>
-  <!-- /wp:heading -->
+    <!-- wp:paragraph -->
+    <p>Kappale: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <!-- /wp:paragraph -->
 
-  <!-- wp:paragraph -->
-  <p>Kappale: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-  <!-- /wp:paragraph -->
+    <!-- wp:heading {"level":3} -->
+    <h3 id="h3-otsikko">H3 Otsikko potsikko pusikko asiaton loton poton pytyn</h3>
+    <!-- /wp:heading -->
 
-  <!-- wp:heading {"level":4} -->
-  <h4 id="h4-otsikko">H4 Otsikko potsikko pusikko asiaton loton poton pytyn Otsikko potsikko pusikko asiaton loton poton pytyn</h4>
-  <!-- /wp:heading -->
+    <!-- wp:paragraph -->
+    <p>Kappale: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <!-- /wp:paragraph -->
 
-  <!-- wp:paragraph -->
-  <p>Kappale: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-  <!-- /wp:paragraph -->
+    <!-- wp:heading {"level":4} -->
+    <h4 id="h4-otsikko">H4 Otsikko potsikko pusikko asiaton loton poton pytyn Otsikko potsikko pusikko asiaton loton poton pytyn</h4>
+    <!-- /wp:heading -->
 
-  <!-- wp:heading {"level":5} -->
-  <h5 id="h5-otsikko">H5 Otsikko potsikko pusikko asiaton loton poton pytyn Otsikko potsikko pusikko asiaton loton poton pytyn</h5>
-  <!-- /wp:heading -->
+    <!-- wp:paragraph -->
+    <p>Kappale: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <!-- /wp:paragraph -->
 
-  <!-- wp:paragraph -->
-  <p>Kappale: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-  <!-- /wp:paragraph -->
+    <!-- wp:heading {"level":5} -->
+    <h5 id="h5-otsikko">H5 Otsikko potsikko pusikko asiaton loton poton pytyn Otsikko potsikko pusikko asiaton loton poton pytyn</h5>
+    <!-- /wp:heading -->
 
-  <!-- wp:heading {"level":6} -->
-  <h6 id="h6-otsikko">H6 Otsikko potsikko pusikko asiaton loton poton pytyn Otsikko potsikko pusikko asiaton loton poton pytyn</h6>
-  <!-- /wp:heading -->
+    <!-- wp:paragraph -->
+    <p>Kappale: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <!-- /wp:paragraph -->
 
-  <!-- wp:paragraph -->
-  <p>Kappale: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-  <!-- /wp:paragraph -->
+    <!-- wp:heading {"level":6} -->
+    <h6 id="h6-otsikko">H6 Otsikko potsikko pusikko asiaton loton poton pytyn Otsikko potsikko pusikko asiaton loton poton pytyn</h6>
+    <!-- /wp:heading -->
 
-  <!-- wp:paragraph -->
-  <p>Kappale: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-  <!-- /wp:paragraph -->
+    <!-- wp:paragraph -->
+    <p>Kappale: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <!-- /wp:paragraph -->
 
-  <!-- wp:paragraph -->
-  <p>Kappale: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-  <!-- /wp:paragraph -->
+    <!-- wp:paragraph -->
+    <p>Kappale: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <!-- /wp:paragraph -->
 
-  <!-- wp:paragraph {"fontSize":"xs"} -->
-  <p class="has-xs-font-size">XS: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-  <!-- /wp:paragraph -->
+    <!-- wp:paragraph -->
+    <p>Kappale: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <!-- /wp:paragraph -->
 
-  <!-- wp:paragraph {"fontSize":"2-xs"} -->
-  <p class="has-2-xs-font-size">2XS: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-  <!-- /wp:paragraph -->
+    <!-- wp:paragraph {"fontSize":"xs"} -->
+    <p class="has-xs-font-size">XS: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <!-- /wp:paragraph -->
+
+    <!-- wp:paragraph {"fontSize":"2-xs"} -->
+    <p class="has-2-xs-font-size">2XS: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <!-- /wp:paragraph -->
+
+  </div>
+  <!-- /wp:group -->
 
 </div>
 <!-- /wp:group -->
