574771915f2af00385a3cbf19b2f6b928f194ae7
Olli <ronkolli@gmail.com>
2023-02-12 14:32:45 +0200

    Added 404 text content

---
+++ patterns/404.php
@@ -0,0 +1,28 @@
+
+<?php
+  /**
+   * Title: 404 heading and paragraph
+   * slug: kide/404
+   * Categories: text
+   * */
+?>
+
+<!-- wp:group {"className":"kide-404-outer-container","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center"}} -->
+<div class="wp-block-group kide-404-outer-container">
+
+  <!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|20","right":"var:preset|spacing|20","bottom":"var:preset|spacing|20","left":"var:preset|spacing|20"},"blockGap":"0"}},"layout":{"type":"constrained","contentSize":"1200px"}} -->
+  <div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)">
+      
+    <!-- wp:heading {"level":1,"className":"kide-404-heading","fontSize":"5-xl","layout":{"type":"default"}} -->
+    <h1 class="kide-404-heading has-5-xl-font-size" id="404">404</h1>
+    <!-- /wp:heading -->
+  
+    <!-- wp:paragraph {"fontSize":"xl"} -->
+    <p class="has-xl-font-size">This is bad news. Looks like that page doesn't exist. But here is the good news. There could be other pages that do exist.</p>
+    <!-- /wp:paragraph -->
+
+  </div>
+  <!-- /wp:group -->
+
+</div>
+<!-- /wp:group -->
