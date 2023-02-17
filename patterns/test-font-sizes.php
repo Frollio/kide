a12b517143f43441b83dce3e9c40f4be94d0029c
Olli <ronkolli@gmail.com>
2023-02-17 20:24:11 +0200

    Added three new preset font sizes, made all font sizes grow linearly

--- patterns/test-font-sizes.php
+++ patterns/test-font-sizes.php
@@ -13,8 +13,32 @@
   <!-- wp:group {"className":"kide-test-font-container","layout":{"type":"constrained"}} -->
   <div class="wp-block-group kide-test-font-container">
     
-    <!-- wp:heading {"fontSize":"5-xl"} -->
-    <h2 class="has-5-xl-font-size" id="h1-5xl-otsikko">H1 5XL Otsikko potsikko pusikko asiaton loton poton pytyn</h2>
+    <!-- wp:heading {"level":1,"fontSize":"8-xl"} -->
+    <h1 class="has-8-xl-font-size" id="h1-8xl-otsikko">H1 8XL Otsikko potsikko pusikko asiaton loton poton pytyn</h2>
+    <!-- /wp:heading -->
+
+    <!-- wp:paragraph -->
+    <p>Kappale: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <!-- /wp:paragraph -->
+
+    <!-- wp:heading {"level":1,"fontSize":"7-xl"} -->
+    <h1 class="has-7-xl-font-size" id="h1-7xl-otsikko">H1 7XL Otsikko potsikko pusikko asiaton loton poton pytyn</h2>
+    <!-- /wp:heading -->
+
+    <!-- wp:paragraph -->
+    <p>Kappale: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <!-- /wp:paragraph -->
+
+    <!-- wp:heading {"level":1,"fontSize":"6-xl"} -->
+    <h1 class="has-6-xl-font-size" id="h1-6xl-otsikko">H1 6XL Otsikko potsikko pusikko asiaton loton poton pytyn</h2>
+    <!-- /wp:heading -->
+
+    <!-- wp:paragraph -->
+    <p>Kappale: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <!-- /wp:paragraph -->
+
+    <!-- wp:heading {"level":1,"fontSize":"5-xl"} -->
+    <h1 class="has-5-xl-font-size" id="h1-5xl-otsikko">H1 5XL Otsikko potsikko pusikko asiaton loton poton pytyn</h2>
     <!-- /wp:heading -->
 
     <!-- wp:paragraph -->
