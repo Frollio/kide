ac20d16334852537de2a42c41eefa34f4de0ef94
Olli <ronkolli@gmail.com>
2023-02-18 16:07:29 +0200

    Refactored styles, edited test content

--- patterns/404.php
+++ patterns/404.php
@@ -7,18 +7,18 @@
    * */
 ?>
 
-<!-- wp:group {"className":"kide-404-outer-container","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center"}} -->
-<div class="wp-block-group kide-404-outer-container">
-
-  <!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|20","right":"var:preset|spacing|20","bottom":"var:preset|spacing|20","left":"var:preset|spacing|20"},"blockGap":"0"}},"layout":{"type":"constrained","contentSize":"1200px"}} -->
-  <div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)">
-      
-    <!-- wp:heading {"level":1,"className":"kide-404-heading","fontSize":"5-xl","layout":{"type":"default"}} -->
-    <h1 class="kide-404-heading has-5-xl-font-size" id="404">404</h1>
-    <!-- /wp:heading -->
+<!-- wp:group {"align":"full","className":"kide-404-outer-container","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull kide-404-outer-container">
   
-    <!-- wp:paragraph {"fontSize":"xl"} -->
-    <p class="has-xl-font-size">This is bad news. Looks like that page doesn't exist. But here is the good news. There could be other pages that do exist.</p>
+  <!-- wp:group {"style":{"spacing":{"padding":{"top":"2rem","right":"2rem","bottom":"2rem","left":"2rem"},"blockGap":"0"}},"layout":{"type":"constrained","contentSize":""}} -->
+  <div class="wp-block-group" style="padding-top:2rem;padding-right:2rem;padding-bottom:2rem;padding-left:2rem">
+  
+    <!-- wp:heading {"textAlign":"center","level":1,"className":"kide-404-heading","fontSize":"5-xl"} -->
+    <h1 class="has-text-align-center kide-404-heading has-5-xl-font-size" id="404">404</h1>
+    <!-- /wp:heading -->
+
+    <!-- wp:paragraph {"align":"center","fontSize":"xl"} -->
+    <p class="has-text-align-center has-xl-font-size">This is bad news. Looks like the page you were looking for doesn't exist. But here is the good news. There could be other pages that do exist.</p>
     <!-- /wp:paragraph -->
 
   </div>
