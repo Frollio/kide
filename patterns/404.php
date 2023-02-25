8c48cd814ad5ab7a208ad8c65a2a2ce32ad9dc22
Olli <ronkolli@gmail.com>
2023-02-25 14:48:11 +0200

    Refactored spacing styles

--- patterns/404.php
+++ patterns/404.php
@@ -10,15 +10,15 @@
 <!-- wp:group {"align":"full","className":"kide-404-outer-container","layout":{"type":"constrained"}} -->
 <div class="wp-block-group alignfull kide-404-outer-container">
   
-  <!-- wp:group {"style":{"spacing":{"padding":{"top":"2rem","right":"2rem","bottom":"2rem","left":"2rem"},"blockGap":"0"}},"layout":{"type":"constrained","contentSize":""}} -->
-  <div class="wp-block-group" style="padding-top:2rem;padding-right:2rem;padding-bottom:2rem;padding-left:2rem">
+  <!-- wp:group {"style":{"spacing":{"blockGap":"0","padding":{"top":"var:preset|spacing|40","right":"var:preset|spacing|40","bottom":"var:preset|spacing|40","left":"var:preset|spacing|40"}}},"layout":{"type":"constrained","contentSize":""}} -->
+  <div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--40)">
   
-    <!-- wp:heading {"textAlign":"center","level":1,"className":"kide-404-heading","fontSize":"5-xl"} -->
-    <h1 class="has-text-align-center kide-404-heading has-5-xl-font-size" id="404">404</h1>
+    <!-- wp:heading {"textAlign":"center","level":1,"className":"kide-404-heading","fontSize":"8-xl"} -->
+    <h1 class="has-text-align-center kide-404-heading has-8-xl-font-size" id="404">404</h1>
     <!-- /wp:heading -->
 
     <!-- wp:paragraph {"align":"center","fontSize":"xl"} -->
-    <p class="has-text-align-center has-xl-font-size">This is bad news. Looks like the page you were looking for doesn't exist. But here is the good news. There could be other pages that do exist.</p>
+    <p class="has-text-align-center has-xl-font-size">This is bad news. Looks like the page you are looking for doesn't exist. But here is the good news. There could be other pages that do exist.</p>
     <!-- /wp:paragraph -->
 
   </div>
