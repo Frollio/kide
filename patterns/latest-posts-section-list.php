c1fefdadb4e6866764052bd8a2f66367b62b7e43
Olli <ronkolli@gmail.com>
2023-02-25 14:13:23 +0200

    Refactored spacing styles

--- patterns/latest-posts-section-list.php
+++ patterns/latest-posts-section-list.php
@@ -10,8 +10,8 @@
 <!-- wp:group {"tagName":"section","align":"full","backgroundColor":"dark-3","layout":{"type":"constrained","justifyContent":"center"}} -->
 <section class="wp-block-group alignfull has-dark-3-background-color has-background">
   
-  <!-- wp:group {"style":{"spacing":{"padding":{"top":"2rem","right":"2rem","bottom":"2rem","left":"2rem"}}},"className":"kide-latest-list-stack","layout":{"type":"flex","orientation":"vertical"}} -->
-  <div class="wp-block-group kide-latest-list-stack" style="padding-top:2rem;padding-right:2rem;padding-bottom:2rem;padding-left:2rem">
+  <!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|40","right":"var:preset|spacing|40","bottom":"var:preset|spacing|40","left":"var:preset|spacing|40"}}},"className":"kide-latest-list-stack","layout":{"type":"flex","orientation":"vertical"}} -->
+  <div class="wp-block-group kide-latest-list-stack" style="padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--40)">
 
     <!-- wp:heading -->
     <h2 id="latest-posts-list-heading">Latest posts</h2>
--- patterns/post.php
+++ patterns/post.php
@@ -7,15 +7,15 @@
    * */
 ?>
 
-<!-- wp:group {"tagName":"section","style":{"spacing":{"padding":{"top":"2rem","right":"2rem","bottom":"2rem","left":"2rem"}}},"className":"kide-post","layout":{"type":"constrained","contentSize":"1200px"}} -->
-<section class="wp-block-group kide-post" style="padding-top:2rem;padding-right:2rem;padding-bottom:2rem;padding-left:2rem">
+<!-- wp:group {"tagName":"section","style":{"spacing":{"padding":{"top":"var:preset|spacing|20","right":"var:preset|spacing|20","bottom":"var:preset|spacing|20","left":"var:preset|spacing|20"}}},"className":"kide-post","layout":{"type":"constrained","contentSize":"1200px"}} -->
+<section class="wp-block-group kide-post" style="padding-top:var(--wp--preset--spacing--20);padding-right:var(--wp--preset--spacing--20);padding-bottom:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--20)">
   
   <!-- wp:post-title {"level":1} /-->
 
   <!-- wp:group {"className":"kide-post-author-row","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"left"}} -->
   <div class="wp-block-group kide-post-author-row">
 
-    <!-- wp:post-author {"showAvatar":false,"showBio":false} /-->
+    <!-- wp:post-author {"showAvatar":false,"showBio":false,"style":{"spacing":{"margin":{"right":"var:preset|spacing|20"}}}} /-->
     <!-- wp:post-date /-->
 
   </div>
