61f7dd73cd7b9e24d3e4518f024bf1cbe54b11fa
Olli <ronkolli@gmail.com>
2023-03-03 19:28:36 +0200

    Removed heading, added search results title

--- patterns/template-query.php
+++ patterns/template-query.php
@@ -12,10 +12,6 @@
   <!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|40","right":"var:preset|spacing|40","bottom":"var:preset|spacing|40","left":"var:preset|spacing|40"}}},"className":"alignwide"} -->
   <div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--40)">
 
-    <!-- wp:heading {"textAlign":"center","level":1,"style":{"spacing":{"margin":{"bottom":"var:preset|spacing|40"}}}} -->
-    <h1 class="has-text-align-center" id="search-results" style="margin-bottom:var(--wp--preset--spacing--40)">Search results</h1>
-    <!-- /wp:heading -->
-
     <!-- wp:query {"queryId":0,"query":{"perPage":3,"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":true},"displayLayout":{"type":"flex","columns":3},"align":"wide","layout":{"type":"default"}} -->
     <div class="wp-block-query alignwide">
 
