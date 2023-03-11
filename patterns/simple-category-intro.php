c80c9ae25bddba359954214d99e83a1695aadd68
Olli <ronkolli@gmail.com>
2023-03-11 10:22:11 +0200

    Replaced simple intro with simple category intro, added template query pattern

---
+++ patterns/simple-category-intro.php
@@ -0,0 +1,25 @@
+
+<?php
+/**
+ * Title: Simple Category Page Introduction
+ * Slug: kide/simple-category-intro
+ * Categories: text
+ */
+?>
+
+<!-- wp:group {"align":"full","backgroundColor":"dark-3","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull has-dark-3-background-color has-background">
+  <!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|40","right":"var:preset|spacing|40","bottom":"var:preset|spacing|40","left":"var:preset|spacing|40"}}},"layout":{"type":"constrained"}} -->
+  <div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--40)">
+
+    <!-- wp:query-title {"type":"archive","textAlign":"center","showPrefix":false,"style":{"spacing":{"margin":{"top":"0"}}}} /-->
+
+    <!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"bottom":"0"}}}} -->
+    <p class="has-text-align-center" style="margin-bottom:0">Here you can tell what this category is about. If you wish, you can create custom category templates for different categories. Copy this template and rename it as category-name.html. For example, category-news.html, if the name of the category is news.
+    </p>
+    <!-- /wp:paragraph -->
+
+  </div>
+  <!-- /wp:group -->
+</div>
+<!-- /wp:group -->
