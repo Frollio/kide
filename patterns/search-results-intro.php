38c3c00549cd268b7ba7c3f942f0b066e1cf1063
Olli <ronkolli@gmail.com>
2023-03-14 18:05:18 +0200

    Added search results intro pattern

---
+++ patterns/search-results-intro.php
@@ -0,0 +1,20 @@
+
+<?php
+/**
+ * Title: Simple search results introduction
+ * Slug: kide/search-results-intro
+ * Categories: text
+ */
+?>
+
+<!-- wp:group {"align":"full","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull">
+  <!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|40","right":"var:preset|spacing|40","bottom":"var:preset|spacing|40","left":"var:preset|spacing|40"}}},"layout":{"type":"constrained"}} -->
+  <div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--40)">
+
+    <!-- wp:query-title {"type":"search","textAlign":"center","style":{"spacing":{"margin":{"top":"0"}}}} /-->
+
+  </div>
+  <!-- /wp:group -->
+</div>
+<!-- /wp:group -->
