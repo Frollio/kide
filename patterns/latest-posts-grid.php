465fc79a6714f6851ffe0da2bbfd837e4d5f1a83
Olli <ronkolli@gmail.com>
2023-03-17 20:08:26 +0200

    Decreased number of columns, removed margin

--- patterns/latest-posts-grid.php
+++ patterns/latest-posts-grid.php
@@ -12,11 +12,11 @@
   <!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|40","right":"var:preset|spacing|40","bottom":"var:preset|spacing|40","left":"var:preset|spacing|40"}}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
   <div class="wp-block-group alignwide kide-latest-list-stack" style="padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--40)">
 
-    <!-- wp:heading {"style":{"spacing":{"margin":{"bottom":"var:preset|spacing|20"}}}} -->
-    <h2 id="latest-posts-list-heading" style="margin-bottom:var(--wp--preset--spacing--20)">Latest posts</h2>
+    <!-- wp:heading -->
+    <h2 id="latest-posts-list-heading">Latest posts</h2>
     <!-- /wp:heading -->
 
-    <!-- wp:query {"queryId":0,"query":{"perPage":5,"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false},"displayLayout":{"type":"flex","columns":3},"className":"kide-query-loop","layout":{"type":"default"}} -->
+    <!-- wp:query {"queryId":0,"query":{"perPage":5,"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false},"displayLayout":{"type":"flex","columns":2},"className":"kide-query-loop","layout":{"type":"default"}} -->
     <div class="wp-block-query kide-query-loop">
       
       <!-- wp:post-template {"className":"kide-post-template"} -->
