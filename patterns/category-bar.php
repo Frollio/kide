ea08829d52c6ebd17568862416689fe17004dd64
Olli <ronkolli@gmail.com>
2023-03-18 19:12:25 +0200

    Replaced hooks with a post terms block

--- patterns/category-bar.php
+++ patterns/category-bar.php
@@ -7,10 +7,12 @@
    * */
 ?>
 
-<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|10","right":"var:preset|spacing|10","bottom":"var:preset|spacing|10","left":"var:preset|spacing|10"}}},"backgroundColor":"dark-4","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center"}} -->
-<div class="wp-block-group alignfull has-dark-4-background-color has-background" style="padding-top:var(--wp--preset--spacing--10);padding-right:var(--wp--preset--spacing--10);padding-bottom:var(--wp--preset--spacing--10);padding-left:var(--wp--preset--spacing--10)">
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|10","right":"var:preset|spacing|10","bottom":"var:preset|spacing|10","left":"var:preset|spacing|10"}}},"backgroundColor":"dark-6","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center"}} -->
+<div class="wp-block-group alignfull has-dark-6-background-color has-background" style="padding-top:var(--wp--preset--spacing--10);padding-right:var(--wp--preset--spacing--10);padding-bottom:var(--wp--preset--spacing--10);padding-left:var(--wp--preset--spacing--10)">
   <!-- wp:heading {"level":4} -->
-  <h4><?php echo( esc_html__( implode( ' ,' ,get_the_category( get_post() ) ), "kide/category-bar" ) ) ?></h4>
+  <h4>
+    <!-- wp:post-terms {"term":"category"} /-->
+  </h4>
   <!-- /wp:heading -->
 </div>
 <!-- /wp:group -->
