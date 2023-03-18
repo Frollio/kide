2e65cfc4e03c3d78c8d67d9b7f48c72d98eea5cb
Olli <ronkolli@gmail.com>
2023-03-18 14:26:12 +0200

    Added cards pattern

---
+++ patterns/cards.php
@@ -0,0 +1,70 @@
+
+<?php
+  /**
+   * Title: Cards
+   * slug: kide/cards
+   * Categories: text
+   * */
+?>
+
+<!-- wp:group {"align":"full","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull"><!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|40","right":"var:preset|spacing|40","bottom":"var:preset|spacing|40","left":"var:preset|spacing|40"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--40)"><!-- wp:group {"align":"full","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center"}} -->
+<div class="wp-block-group alignfull"><!-- wp:heading -->
+<h2 id="another-set-of-cards">Another set of cards</h2>
+<!-- /wp:heading --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"align":"wide","layout":{"type":"flex","flexWrap":"wrap","justifyContent":"center","verticalAlignment":"top"}} -->
+<div class="wp-block-group alignwide"><!-- wp:group {"layout":{"type":"constrained","contentSize":"400px"}} -->
+<div class="wp-block-group"><!-- wp:heading -->
+<h2 id="column-1">Card 1</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>You place here text about some sub-topic like gardening in Antarctis. You can add an image above or below this text. You can add more cards like this by duplicating this Group.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"constrained","contentSize":"400px"}} -->
+<div class="wp-block-group"><!-- wp:heading -->
+<h2 id="column-1">Card 2</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>You place here text about some sub-topic like gardening in Antarctis. You can add an image above or below this text. You can add more cards like this by duplicating this Group. This column has the longest paragraph. It would look better if you keep columns short and with same length, if possible.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"constrained","contentSize":"400px"}} -->
+<div class="wp-block-group"><!-- wp:heading -->
+<h2 id="column-1">Card 3</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>You place here text about some sub-topic like gardening in Antarctis. You can add an image above or below this text. You can add more cards like this by duplicating this Group.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"constrained","contentSize":"400px"}} -->
+<div class="wp-block-group"><!-- wp:heading -->
+<h2 id="column-1">Card 4</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>You place here text about some sub-topic like gardening in Antarctis. You can add an image above or below this text. You can add more cards like this by duplicating this Group.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"constrained","contentSize":"400px"}} -->
+<div class="wp-block-group"><!-- wp:heading -->
+<h2 id="column-1">Card 5</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>You place here text about some sub-topic like gardening in Antarctis. You can add an image above or below this text. You can add more cards like this by duplicating this Group.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
