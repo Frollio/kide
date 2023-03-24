2d02c066863134313a33f65f602e5acf936e482a
Olli <ronkolli@gmail.com>
2023-03-24 19:09:51 +0200

    Replaced custom HTML with an image block

---
+++ patterns/search-toggle.php
@@ -0,0 +1,14 @@
+
+<?php
+/**
+ * Title: Search toggle button for Kide theme
+ * Slug: kide/search-toggle
+ * Categories: buttons
+ */
+?>
+
+<!-- wp:image {"width":40,"height":40,"sizeSlug":"large","className":"kide-search-button"} -->
+<figure class="wp-block-image size-large is-resized kide-search-button" id="kide-search-toggle">
+  <img src="<?php echo esc_url( get_theme_file_uri( 'assets/images/magnifying-glass-white.svg' ) )?>" alt="Magnifying glass" width="40" height="40"/>
+</figure>
+<!-- /wp:image -->
