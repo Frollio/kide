01ec2ea262e53ae3af1aa158df900640618e4c33
Olli <ronkolli@gmail.com>
2023-03-26 14:08:08 +0300

    Set search toggle button's height fluid

--- patterns/search-toggle.php
+++ patterns/search-toggle.php
@@ -7,8 +7,8 @@
  */
 ?>
 
-<!-- wp:image {"width":40,"height":40,"sizeSlug":"large","className":"kide-search-button"} -->
-<figure class="wp-block-image size-large is-resized kide-search-button" id="kide-search-toggle">
+<!-- wp:image {"sizeSlug":"large","className":"kide-search-button","kide-search-toggle-height"} -->
+<figure class="wp-block-image size-large is-resized kide-search-button kide-search-toggle-height" id="kide-search-toggle">
   <img src="<?php echo esc_url( get_theme_file_uri( 'assets/images/magnifying-glass-white.svg' ) )?>" alt="Magnifying glass" width="40" height="40"/>
 </figure>
 <!-- /wp:image -->
