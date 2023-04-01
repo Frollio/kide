1d822ea672dc8e5e2bde6d673280ae0052370873
Olli <ronkolli@gmail.com>
2023-04-01 15:37:50 +0300

    Added paragraphs row pattern

---
+++ patterns/paragraphs-row.php
@@ -0,0 +1,42 @@
+
+<?php
+  /**
+   * Title: A row with paragraphs
+   * slug: kide/paragraphs-row
+   * Categories: text
+   * */
+?>
+
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group">
+
+<!-- wp:group {"align":"full","backgroundColor":"dark-4","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull has-dark-4-background-color has-background">
+
+<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|40","right":"var:preset|spacing|40","bottom":"var:preset|spacing|40","left":"var:preset|spacing|40"},"blockGap":"var:preset|spacing|40"}},"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"center","verticalAlignment":"top"}} -->
+<div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--40)">
+
+<!-- wp:paragraph {"align":"center","style":{"layout":{"selfStretch":"fixed","flexSize":"300px"}}} -->
+<p class="has-text-align-center">First paragraph in this row</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center","style":{"layout":{"selfStretch":"fixed","flexSize":"300px"}}} -->
+<p class="has-text-align-center">Second paragraph in this row</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center","style":{"layout":{"selfStretch":"fixed","flexSize":"300px"}}} -->
+<p class="has-text-align-center">Third paragraph in this row</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center","style":{"layout":{"selfStretch":"fixed","flexSize":"300px"}}} -->
+<p class="has-text-align-center">Fourth paragraph in this row</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center","style":{"layout":{"selfStretch":"fixed","flexSize":"300px"}}} -->
+<p class="has-text-align-center">The last paragraph in this row</p>
+<!-- /wp:paragraph -->
+
+</div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
