97385ed3c1d39945ee738f8d4bd1792590d355fc
Olli <ronkolli@gmail.com>
2023-04-02 13:13:26 +0300

    Added test patterns for styling blocks

---
+++ patterns/test-design-blocks.php
@@ -0,0 +1,52 @@
+
+<?php
+  /**
+   * Title: Design blocks test
+   * slug: kide/test-design-blocks
+   * Categories: text, image
+   * */
+?>
+
+<!-- wp:group {"align":"full","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|40","right":"var:preset|spacing|40","bottom":"var:preset|spacing|40","left":"var:preset|spacing|40"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--40)"><!-- wp:heading -->
+<h2 class="wp-block-heading" id="widgets-blocks-test">Design blocks</h2>
+<!-- /wp:heading -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button /-->
+
+<!-- wp:button /-->
+
+<!-- wp:button /-->
+
+<!-- wp:button /--></div>
+<!-- /wp:buttons -->
+
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:paragraph -->
+<p>Here is some text.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:paragraph -->
+<p>Here is some text.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:more -->
+<!--more-->
+<!-- /wp:more -->
+
+<!-- wp:separator -->
+<hr class="wp-block-separator has-alpha-channel-opacity"/>
+<!-- /wp:separator -->
+
+<!-- wp:spacer -->
+<div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
---
+++ patterns/test-text-blocks.php
@@ -0,0 +1,56 @@
+
+<?php
+  /**
+   * Title: Text blocks test
+   * slug: kide/test-text-blocks
+   * Categories: text
+   * */
+?>
+
+<!-- wp:group {"align":"full","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|40","right":"var:preset|spacing|40","bottom":"var:preset|spacing|40","left":"var:preset|spacing|40"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--40)"><!-- wp:heading -->
+<h2 class="wp-block-heading" id="text-blocks">Text blocks</h2>
+<!-- /wp:heading -->
+
+<!-- wp:list -->
+<ul><!-- wp:list-item -->
+<li>List item</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>List item</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>List item</li>
+<!-- /wp:list-item --></ul>
+<!-- /wp:list -->
+
+<!-- wp:quote -->
+<blockquote class="wp-block-quote"><!-- wp:paragraph -->
+<p>"Here is some text."</p>
+<!-- /wp:paragraph --><cite>Somebody</cite></blockquote>
+<!-- /wp:quote -->
+
+<!-- wp:code -->
+<pre class="wp-block-code"><code>&lt;html&gt;&lt;/html&gt;</code></pre>
+<!-- /wp:code -->
+
+<!-- wp:preformatted -->
+<pre class="wp-block-preformatted">Preformatted text.</pre>
+<!-- /wp:preformatted -->
+
+<!-- wp:pullquote -->
+<figure class="wp-block-pullquote"><blockquote><p>Here is quote</p><cite>Pullquote: Here is citation</cite></blockquote></figure>
+<!-- /wp:pullquote -->
+
+<!-- wp:table {"hasFixedLayout":true} -->
+<figure class="wp-block-table"><table class="has-fixed-layout"><thead><tr><th>Header 1</th><th>Header 2</th></tr></thead><tbody><tr><td>Information</td><td>Values</td></tr><tr><td>Personality</td><td>Messing everything</td></tr></tbody><tfoot><tr><td></td><td></td></tr></tfoot></table><figcaption class="wp-element-caption">Here is the caption of this table</figcaption></figure>
+<!-- /wp:table -->
+
+<!-- wp:verse -->
+<pre class="wp-block-verse">Verse: Here is some verse.</pre>
+<!-- /wp:verse --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
