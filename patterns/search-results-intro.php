88d38600f0d5d4dd7bbae7369afcd29adc3af83d
Olli <ronkolli@gmail.com>
2023-04-08 10:27:05 +0300

    Search results intro: Added cover block.

--- patterns/search-results-intro.php
+++ patterns/search-results-intro.php
@@ -7,14 +7,25 @@
  */
 ?>
 
-<!-- wp:group {"align":"full","backgroundColor":"dark-7","layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignfull has-dark-7-background-color has-background">
-  <!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|40","right":"var:preset|spacing|40","bottom":"var:preset|spacing|40","left":"var:preset|spacing|40"}}},"layout":{"type":"constrained"}} -->
-  <div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--40)">
+<!-- wp:cover {"overlayColor":"dark-7","align":"full"} -->
+<div class="wp-block-cover alignfull">
+  <span aria-hidden="true" class="wp-block-cover__background has-dark-7-background-color has-background-dim-100 has-background-dim"></span>
+<div class="wp-block-cover__inner-container">
 
-    <!-- wp:query-title {"type":"search","textAlign":"center","style":{"spacing":{"margin":{"top":"0"}}}} /-->
+  <!-- wp:group {"layout":{"type":"constrained"}} -->
+  <div class="wp-block-group">
+
+    <!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|40","right":"var:preset|spacing|40","bottom":"var:preset|spacing|40","left":"var:preset|spacing|40"}}},"layout":{"type":"constrained"}} -->
+    <div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--40)">
+
+      <!-- wp:query-title {"type":"search","textAlign":"center","style":{"spacing":{"margin":{"top":"0"}}}} /-->
+
+    </div>
+    <!-- /wp:group -->
 
   </div>
   <!-- /wp:group -->
+
 </div>
-<!-- /wp:group -->
+</div>
+<!-- /wp:cover -->
