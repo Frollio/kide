b4f052626580c916db0beb0bb62d485aa092feb4
Olli <ronkolli@gmail.com>
2023-07-15 10:50:19 +0300

    Paragraphs row pattern: Deleted excessive Group block.

--- patterns/paragraphs-row.php
+++ patterns/paragraphs-row.php
@@ -7,11 +7,8 @@
    * */
 ?>
 
-<!-- wp:group {"tagName":"section","layout":{"type":"constrained"}} -->
-<section class="wp-block-group">
-
-  <!-- wp:group {"align":"full","backgroundColor":"dark-4","layout":{"type":"constrained"}} -->
-  <div class="wp-block-group alignfull has-dark-4-background-color has-background">
+<!-- wp:group {"tagName":"section","align":"full","backgroundColor":"dark-4","layout":{"type":"constrained"}} -->
+<section class="wp-block-group alignfull has-dark-4-background-color has-background">
 
     <!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|40","right":"var:preset|spacing|20","bottom":"var:preset|spacing|40","left":"var:preset|spacing|20"},"blockGap":"var:preset|spacing|40"}},"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"center","verticalAlignment":"top"}} -->
     <div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--20);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--20)">
@@ -38,9 +35,6 @@
 
     </div>
     <!-- /wp:group -->
-
-  </div>
-  <!-- /wp:group -->
 
 </section>
 <!-- /wp:group -->
