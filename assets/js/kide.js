29e09738f3996ce28f70272d9e22533b0302fede
Olli <ronkolli@gmail.com>
2024-06-30 14:04:50 +0300

    Fixed the focus of the search field when the user opens the search bar.

--- assets/js/kide.js
+++ assets/js/kide.js
@@ -2,5 +2,5 @@
 document.getElementById('kide-search-toggle').onclick = function() {
     const elem = document.getElementById('kide-search-bar');
     elem.classList.toggle('kide-is-block');
-    document.getElementById('wp-block-search__input-4').focus();
+    document.getElementById('wp-block-search__input-2').focus();
 }
